a1f1621e4b291ab18108ceaba01e853953acc334
risangdevs <88659300+risangdevs@users.noreply.github.com>
2023-03-22 17:48:17 +0700

    CRUD done

--- crud/app.js
+++ crud/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const Item = require("./models/items");
-const {connect}=require('./config/mongo')
+const { connect } = require("./config/mongo");
 
+app.use(express.json());
 // Routes for CRUD operations
 app.get("/items", (req, res) => {
   // Retrieve all items from the database
@@ -13,25 +14,45 @@ app.get("/items", (req, res) => {
     .catch((_) => console.log(_));
 });
 
+app.get("/items/:id", (req, res) => {
+  // Retrieve one item from the database
+  Item.findByPk(req.params.id)
+    .then((items) => {
+      res.status(200).json(items);
+    })
+    .catch((_) => console.log(_));
+});
+
 app.post("/items", (req, res) => {
   // Create a new item in the database
-  console.log("create item");
+  Item.add(req.body)
+    .then((items) => {
+      res.status(200).json(items);
+    })
+    .catch((_) => console.log(_));
 });
 
 app.put("/items/:id", (req, res) => {
   // Update an existing item in the database
-  console.log("update item");
+  Item.edit(req.params.id, req.body)
+    .then((items) => {
+      res.status(200).json(items);
+    })
+    .catch((_) => console.log(_));
 });
 
 app.delete("/items/:id", (req, res) => {
   // Delete an item from the database
-  console.log("delete item");
+  Item.delete(req.params.id)
+    .then((items) => {
+      res.status(200).json(items);
+    })
+    .catch((_) => console.log(_));
 });
-
 
 connect()
   .then(async () => {
-    app.listen(3000,() => {
+    app.listen(3000, () => {
       console.log("CRUD microservice listening on port 3000!");
     });
   })
